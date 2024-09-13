9deb7cf3a57d0b4e9f6fa4f4c3411f82427bc9d2
Arnav Shah <shah.h.arnav@gmail.com>
2024-09-13 12:21:49 -0700

    Solved BasicBank

--- BasicBank/src/BasicBank.sol
+++ BasicBank/src/BasicBank.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
+// call is the current recommended method to send ether (https://solidity-by-example.org/sending-ether/)
+
 contract BasicBank {
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
     function addEther() external payable {}
 
     /// @notice used to withdraw ether from the contract (No restriction on withdrawals)
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+        (bool sent, ) = msg.sender.call{value:amount}("");
+        require(sent, "Failed to send ether");
+    }
 }
