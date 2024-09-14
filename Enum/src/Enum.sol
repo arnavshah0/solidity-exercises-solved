a561af048259803da6c8d42ef7e7d4f55efbbed2
Arnav Shah <shah.h.arnav@gmail.com>
2024-09-13 17:53:46 -0700

    Enum Solved.

--- Enum/src/Enum.sol
+++ Enum/src/Enum.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
+// You must cast integers to enums to get the corresponding enum value.
+// 
+
 contract ExampleEnum {
     /*
         This exercise assumes you understand how Enum works.
@@ -18,6 +21,11 @@ contract ExampleEnum {
     }
 
     function isWeekend(uint256 index) public pure returns (bool) {
-        // your code here
+        require(index < 7, "Invalid Index");
+        DayOfWeek day = DayOfWeek(index);
+        if (day == DayOfWeek.Saturday || day == DayOfWeek.Sunday) {
+            return true;
+        }
+        return false;
     }
 }
